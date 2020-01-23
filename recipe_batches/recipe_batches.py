0d9f31f6c95196ffc23e07f0540ff130197b0f14
greenhornsr <srollins11279@gmail.com>
2020-01-23 14:35:30 -0800

    changed knapsack directory name, initial pass of recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,9 +2,23 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
 
+# for each key in recipe, ingredient.key // ingredient.key, 
+  # if all results > 0, return highest common result
+
+def recipe_batches(recipe, ingredients):
+  current_qty = 0
+  batches = 0
+  for val1 in recipe.values():
+    for val2 in ingredients.values():
+      if (val2 // val1) > 0:
+        current_qty = val2 // val1
+      if current_qty < batches: 
+        batches = current_qty
+
+recipe_batches(recipe, ingredients)
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
